480c62a6bd582d6571c3512c2e0d5f9dd58a11e9
Adesh Kumar C T <adeshct@gmail.com>
2025-01-30 21:35:26 +0530

    Update StartAlgoAPI.py

--- restapis/StartAlgoAPI.py
+++ restapis/StartAlgoAPI.py
@@ -4,14 +4,16 @@ import logging
 import threading
 from config.Config import getSystemConfig
 from core.Algo import Algo
+from utils.Utils import Utils
 
 class StartAlgoAPI(MethodView):
   def post(self):
     # start algo in a separate thread
+    server_ip = Utils.get_sever_ip()
     x = threading.Thread(target=Algo.startAlgo)
     x.start()
     systemConfig = getSystemConfig()
-    homeUrl = systemConfig['homeUrl'] + '?algoStarted=true'
+    homeUrl = 'http://' + server_ip + ':8080/?algoStarted=true'
     logging.info('Sending redirect url %s in response', homeUrl)
     respData = { 'redirect': homeUrl }
     return json.dumps(respData)
