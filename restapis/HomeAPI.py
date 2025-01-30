3b10cf7e748f8c7e859cd7ccd6156d9b0d788a92
Adesh Kumar C T <adeshct@gmail.com>
2025-01-30 21:31:08 +0530

    Update HomeAPI.py

--- restapis/HomeAPI.py
+++ restapis/HomeAPI.py
@@ -2,20 +2,10 @@ from flask.views import MethodView
 from flask import render_template, request
 import socket
 import requests
+from utils.Utils import get_server_ip
 
 class HomeAPI(MethodView):
   def get(self):
-    def get_server_ip():
-      """ Fetch the public IP of the server. """
-      try:
-        # AWS metadata service for private IP
-        private_ip = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4", timeout=2).text
-        # Public IP (if required)
-        public_ip = requests.get("https://api.ipify.org?format=json", timeout=2).json().get("ip")
-        return public_ip  # Use public_ip if you need an external-facing IP
-      except Exception:
-        return socket.gethostbyname(socket.gethostname())  # Fallback to local IP
-        
     server_ip =  get_server_ip()
     
     if 'loggedIn' in request.args and request.args['loggedIn'] == 'true':
